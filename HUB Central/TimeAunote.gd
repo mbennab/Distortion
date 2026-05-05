7d7cc55f6726584fa31b2c2adadf5198a9b4d7f6
alixdelecour <alixdelecour@gmail.com>
2026-05-05 10:49:37 +0200

    changement textures perso

--- HUB Central/TimeAunote.gd
+++ HUB Central/TimeAunote.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var timeAunoteAnimation
 var timeAunoteCollision
 var timerAttente
+var direction = "bas"
 
 func _ready():
 	hide()
@@ -19,27 +20,45 @@ func _process(delta):
 	pass
 
 func apparition(position):
-	timeAunoteAnimation.animation = "repos"
+	timeAunoteAnimation.animation = "idle_face"
 	self.position = position
 	show()
 	timeAunoteCollision.disabled = false
 
 func animation(mouvement):
 	if mouvement == Vector2.ZERO:
-		if timerAttente.is_stopped():
-			timeAunoteAnimation.animation = "attente"
-		else:
-			timeAunoteAnimation.animation = "repos"
-		return
+		match direction:
+			"bas":
+				timeAunoteAnimation.animation = "idle_face"
+			"haut":
+				timeAunoteAnimation.animation = "idle_dos"
+			"droite":
+				timeAunoteAnimation.animation = "idle_cote"
+				timeAunoteAnimation.flip_h = false
+			"gauche":
+				timeAunoteAnimation.animation = "idle_cote"
+				timeAunoteAnimation.flip_h = true
+	
+	#if mouvement == Vector2.ZERO:
+		#if timerAttente.is_stopped():
+			#timeAunoteAnimation.animation = "attente"
+		#else:
+			#timeAunoteAnimation.animation = "repos"
+		#return
+	
 	if mouvement.x != 0:
-		timeAunoteAnimation.animation = "marche_droite"
+		timeAunoteAnimation.animation = "marche_cote"
 		if mouvement.x > 0:
-			timeAunoteAnimation.flip_h = false
-		else:
 			timeAunoteAnimation.flip_h = true
+			direction = "gauche"
+		else:
+			timeAunoteAnimation.flip_h = false
+			direction = "droite"
 	if mouvement.y != 0:
 		if mouvement.y > 0:
-			timeAunoteAnimation.animation = "marche_bas"
+			timeAunoteAnimation.animation = "marche_face"
+			direction = "bas"
 		else:
-			timeAunoteAnimation.animation = "marche_haut"
+			timeAunoteAnimation.animation = "marche_dos"
+			direction = "haut"
 	timerAttente.start()
