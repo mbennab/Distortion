640ee0fa17aabf826ca97d722b0a56a3b0b06b2e
Rimbaud1 <rimbaud.boniface@student.junia.com>
2026-05-22 09:45:23 +0200

    style: change dog interaction prompt to 'jouer avec le chien'

--- HUB Central/chien_hub.gd
+++ HUB Central/chien_hub.gd
@@ -74,7 +74,7 @@ func _setup_bubble() -> void:
 
 func _setup_prompt() -> void:
 	_prompt_label = Label.new()
-	_prompt_label.text = "Appuyez sur E pour dire bonjour au chien"
+	_prompt_label.text = "Appuyez sur E pour jouer avec le chien"
 	_prompt_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	_prompt_label.add_theme_font_size_override("font_size", 26)
 	_prompt_label.add_theme_color_override("font_color", Color(1, 1, 1, 0.8))
